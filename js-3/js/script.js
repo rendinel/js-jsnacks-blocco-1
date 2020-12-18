8eb4741c6e7ab8a6c2715df9ce1b086674ab238f
lucio rendine <luciorendine69@gmail.com>
2020-12-18 16:49:33 +0100

    third snack

--- js-3/js/script.js
+++ js-3/js/script.js
@@ -1,13 +1,19 @@
-var firstWord = parseInt(prompt('Inserisci un numero'));
-var secondWord = parseInt(prompt('Inserisci un secondo numero'));
-var thirdWord = parseInt(prompt('Inserisci un terzo numero'));
-var fourthWord = parseInt(prompt('Inserisci un quarto numero'));
-var fifthWord = parseInt(prompt('Inserisci un quinto numero'));
-var sumWordArray = ["firstWord", "secondWord", "thirdWord", "fourthWord" , "fifthWord" ]
-// var sumWord = firstWord + secondWord + thirdWord + fourthWord + fifthWord;
-// console.log('La somma é:' + sumWord);;
+var listaN = [];
+var n1 = prompt('Inserisci un numero');
+listaN.push(parseInt(n1));
+var n2 = prompt('Inserisci un secondo numero');
+listaN.push(parseInt(n2));
+var n3 = prompt('Inserisci un terzo numero');
+listaN.push(parseInt(n3));
+var n4 = prompt('Inserisci un quarto numero');
+listaN.push(parseInt(n4));
+var n5 = prompt('Inserisci un quinto numero');
+listaN.push(parseInt(n5));
 
-var i;
-for (i = 0; sumWordArray[i]  ; i++) {
-  text += sumWordArray[i] + sumWordArray[i] + sumWordArray[i] + sumWordArray[i] + sumWordArray[i] ;
+var somma = 0;
+
+for (var x = 0; x < listaN.length; x++) {
+  somma += listaN[x];
 }
+
+console.log('La tua somma é ' + somma)
