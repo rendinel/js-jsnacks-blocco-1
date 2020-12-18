1124743ba3a85711926befce26482c48b7dbc545
lucio rendine <luciorendine69@gmail.com>
2020-12-18 17:31:29 +0100

    third snack short version for

--- js-3/js/script.js
+++ js-3/js/script.js
@@ -1,19 +1,30 @@
-var listaN = [];
-var n1 = prompt('Inserisci un numero');
-listaN.push(parseInt(n1));
-var n2 = prompt('Inserisci un secondo numero');
-listaN.push(parseInt(n2));
-var n3 = prompt('Inserisci un terzo numero');
-listaN.push(parseInt(n3));
-var n4 = prompt('Inserisci un quarto numero');
-listaN.push(parseInt(n4));
-var n5 = prompt('Inserisci un quinto numero');
-listaN.push(parseInt(n5));
+// versione lunga
+// var listaN = [];
+// var n1 = prompt('Inserisci un numero');
+// listaN.push(parseInt(n1));
+// var n2 = prompt('Inserisci un secondo numero');
+// listaN.push(parseInt(n2));
+// var n3 = prompt('Inserisci un terzo numero');
+// listaN.push(parseInt(n3));
+// var n4 = prompt('Inserisci un quarto numero');
+// listaN.push(parseInt(n4));
+// var n5 = prompt('Inserisci un quinto numero');
+// listaN.push(parseInt(n5));
+//
+// var somma = 0;
+//
+// for (var x = 0; x < listaN.length; x++) {
+//   somma += listaN[x];
+// }
+//
+// console.log('La tua somma é ' + somma)
 
+// versione stretta perché quando delle cose si ripetono quasi sicuramente si tratta di un ciclo
 var somma = 0;
 
-for (var x = 0; x < listaN.length; x++) {
-  somma += listaN[x];
+for (var x =  0; x < 5; x++){
+  var n = prompt('Inserisci un numero');
+  somma += parseInt(n);
 }
 
 console.log('La tua somma é ' + somma)
