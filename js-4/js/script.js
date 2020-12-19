6b1e43f53495e39f6ed0a7055b19701fb03567e6
lucio rendine <luciorendine69@gmail.com>
2020-12-19 12:26:23 +0100

    fourth snack ok

--- js-4/js/script.js
+++ js-4/js/script.js
@@ -1,16 +1,33 @@
-var nameSurname = prompt('Inserisci il tuo nome');
-var nameSurnameCheck = false;
-var listNameSurname = ['lucio','andrea','francesco'];
-var welcome = document.getElementById('welcome');
+var name = prompt('come ti chiami?');
+var nameList = ['lucio', 'andrea', 'diego'];
+var flag = false;
 
-for(var i = 0; i < listNameSurname.lenght; i++){
-  if (listNameSurname[i] === nameSurname) {
-    nameSurnameCheck = true;
+for (var i = 0; i < nameList.length; i++) {
+  if (name === nameList[i]){
+    flag = true;
   }
 }
 
-if (nameSurnameCheck = true) {
-    welcome.innerText = 'Benvenuto';
-} else {
-    welcome.innerText = 'vai via';
+if ( flag ){
+  console.log('puoi entrare');
+}else{
+  console.log('no'); 
 }
+
+
+// var nameSurname = prompt('Inserisci il tuo nome');
+// var nameSurnameCheck = false;
+// var listNameSurname = ['lucio','andrea','francesco'];
+// var welcome = document.getElementById('welcome');
+//
+// for(var i = 0; i < listNameSurname.lenght; i++){
+//   if ( NameSurname === listNameSurname[i]) {
+//     nameSurnameCheck = true;
+//   }
+// }
+//
+// if (nameSurnameCheck) {
+//     welcome.innerText = 'Benvenuto';
+// } else {
+//     welcome.innerText = 'vai via';
+// }
